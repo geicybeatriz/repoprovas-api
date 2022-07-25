09d9f9e272cb964ddd375550c2050647b11b9fa1
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-07-25 10:45:20 -0300

    tests: create and get tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -9,6 +9,15 @@ const EMAIL = "teste1@gmail.com";
 const PASSWORD = "senhaTeste01";
 const userData = { email:EMAIL, password:PASSWORD};
 
+//dados de provas
+const testData ={
+    name: "Apostila teste 1",
+    pdfUrl: "https://http.cat/102",
+    category:"Projeto",
+    discipline: "Humildade",
+    teacher: "Bruna Hamori"
+}
+
 beforeEach(async () => await prisma.$executeRaw`TRUNCATE TABLE users`)
 
 describe("POST /signup", () => {
@@ -64,6 +73,106 @@ describe("POST /signin", () => {
     });
 });
 
-// afterAll(async () => {
-//     await prisma.$disconnect();
-// })
+describe("POST /tests", () => {
+    it("return 201 for create tests successfully", async () => {
+        const createUser = await agent.post("/signup").send({...userData, confirm_password:PASSWORD});
+        expect(createUser.status).toEqual(201);
+
+        const login = await agent.post("/signin").send(userData);
+        const token = login.text;
+        expect(login.status).toBe(200);
+
+        const newPost = await agent.post("/tests").send(testData).set("Authorization", `Bearer ${token}`);
+        expect(newPost.status).toEqual(201);
+    });
+
+    it("return 422 for invalid category", async () => {
+        const createUser = await agent.post("/signup").send({...userData, confirm_password:PASSWORD});
+        expect(createUser.status).toEqual(201);
+
+        const login = await agent.post("/signin").send(userData);
+        const token = login.text;
+        expect(login.status).toBe(200);
+
+        const newPost = await agent.post("/tests").send({...testData, category: "invalid"}).set("Authorization", `Bearer ${token}`);
+        expect(newPost.status).toEqual(422);
+    });
+
+    it("return 422 for invalid teacher", async () => {
+        const createUser = await agent.post("/signup").send({...userData, confirm_password:PASSWORD});
+        expect(createUser.status).toEqual(201);
+
+        const login = await agent.post("/signin").send(userData);
+        const token = login.text;
+        expect(login.status).toBe(200);
+
+        const newPost = await agent.post("/tests").send({...testData, teacher: "invalid"}).set("Authorization", `Bearer ${token}`);
+        expect(newPost.status).toEqual(422);
+    });
+
+    it("return 422 for invalid discipline", async () => {
+        const createUser = await agent.post("/signup").send({...userData, confirm_password:PASSWORD});
+        expect(createUser.status).toEqual(201);
+
+        const login = await agent.post("/signin").send(userData);
+        const token = login.text;
+        expect(login.status).toBe(200);
+
+        const newPost = await agent.post("/tests").send({...testData, discipline: "invalid"}).set("Authorization", `Bearer ${token}`);
+        expect(newPost.status).toEqual(422);
+    });
+
+    it("return 404 for invalid teacherDiscipline", async () => {
+        const createUser = await agent.post("/signup").send({...userData, confirm_password:PASSWORD});
+        expect(createUser.status).toEqual(201);
+
+        const login = await agent.post("/signin").send(userData);
+        const token = login.text;
+        expect(login.status).toBe(200);
+
+        const newPost = await agent.post("/tests").send({...testData, discipline: "Humildade", teacher:"Diego Pinho"}).set("Authorization", `Bearer ${token}`);
+        expect(newPost.status).toEqual(404);
+    });
+});
+
+describe("GET /tests", () => {
+    it("return 200 for find all tests", async () => {
+        const createUser = await agent.post("/signup").send({...userData, confirm_password:PASSWORD});
+        expect(createUser.status).toEqual(201);
+
+        const login = await agent.post("/signin").send(userData);
+        const token = login.text;
+        expect(login.status).toBe(200);
+
+        const newPost = await agent.get("/tests").set("Authorization", `Bearer ${token}`);
+        expect(newPost.status).toEqual(200);
+    });
+
+    it("return 200 for find tests by disciplines", async () => {
+        const createUser = await agent.post("/signup").send({...userData, confirm_password:PASSWORD});
+        expect(createUser.status).toEqual(201);
+
+        const login = await agent.post("/signin").send(userData);
+        const token = login.text;
+        expect(login.status).toBe(200);
+
+        const newPost = await agent.get("/tests?groupBy=discipline").set("Authorization", `Bearer ${token}`);
+        expect(newPost.status).toEqual(200);
+    });
+
+    it("return 200 for find tests by disciplines", async () => {
+        const createUser = await agent.post("/signup").send({...userData, confirm_password:PASSWORD});
+        expect(createUser.status).toEqual(201);
+
+        const login = await agent.post("/signin").send(userData);
+        const token = login.text;
+        expect(login.status).toBe(200);
+
+        const newPost = await agent.get("/tests?groupBy=teacher").set("Authorization", `Bearer ${token}`);
+        expect(newPost.status).toEqual(200);
+    });
+});
+
+afterAll(async () => {
+    await prisma.$disconnect();
+})
